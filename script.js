bbe9c3cb6d3cdb949875117820034e39ff4b6273
unknown <342353570@TDSB.ON.CA>
2024-10-07 11:39:38 -0400

    did some things, gotta go

---
+++ script.js
@@ -0,0 +1,5 @@
+const posterSet1 = ["Movies/fightClub.jpg", "Movies/theMatrix.jpg", "Movies/pulpFiction.jpg"]
+const posterSet2 = ["Movies/forrestGump.jpg", "Movies/theKarateKid.jpg", "Movies/interstellar.jpg"]
+const posterSet3 = ["Movies/furious7.jpg", "Movies/theGodfather.jpg", "Movies/shawshankRedemption.jpg"]
+const posterSet4 = ["Movies/goodfellas.jpg", "Movies/theDarkKnightRises.jpg", "Movies/harryPotterAndTheSorcerersStone.jpg"]
+const posterSet5 = ["Movies/inception.jpg", "Movies/starWarsEpisode4.jpg", "Movies/lordOfTheRings.jpg"]
