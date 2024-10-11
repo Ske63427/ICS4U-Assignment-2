195bb7fecd16be3e91a244af905ff44f527c6e75
Aidan Samaroo <skelecrae63427@gmail.com>
2024-10-11 16:23:48 -0400

    minor changes

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-const POSTER_SET_ONE = ["Movies/theGodfather.jpg","Movies/pulpFiction.jpg","Movies/goodfellas.jpg"];
-const POSTER_SET_TWO = ["Movies/forrestGump.jpg","Movies/furious7.jpg","Movies/interstellar.jpg"];
-const POSTER_SET_THREE = ["Movies/theKarateKid.jpg","Movies/theDarkKnightRises.jpg","Movies/shawshankRedemption.jpg"];
-const POSTER_SET_FOUR = ["Movies/lordOfTheRings.jpg","Movies/starWarsEpisode4.jpg","Movies/harryPotterAndTheSorcerersStone.jpg"];
-const POSTER_SET_FIVE = ["Movies/fightClub.jpg","Movies/theMatrix.jpg","Movies/inception.jpg"];
-const ALL_POSTER_SETS = [POSTER_SET_ONE, POSTER_SET_TWO, POSTER_SET_THREE, POSTER_SET_FOUR, POSTER_SET_FIVE];
+const ALL_POSTER_SETS = [["Movies/theGodfather.jpg","Movies/pulpFiction.jpg","Movies/goodfellas.jpg"], ["Movies/forrestGump.jpg","Movies/furious7.jpg","Movies/interstellar.jpg"], ["Movies/theKarateKid.jpg","Movies/theDarkKnightRises.jpg","Movies/shawshankRedemption.jpg"], ["Movies/lordOfTheRings.jpg","Movies/starWarsEpisode4.jpg","Movies/harryPotterAndTheSorcerersStone.jpg"], ["Movies/fightClub.jpg","Movies/theMatrix.jpg","Movies/inception.jpg"]];
 
-const POSTER_SLOT_ONE = document.getElementById("posterOne");
-const POSTER_SLOT_TWO = document.getElementById("posterTwo");
-const POSTER_SLOT_THREE = document.getElementById("posterThree");
-const ALL_POSTER_SLOTS = [POSTER_SLOT_ONE, POSTER_SLOT_TWO, POSTER_SLOT_THREE]
+const POSTER_SLOTS = [document.getElementById("posterOne"), document.getElementById("posterTwo"), document.getElementById("posterThree")]
 
 const LEFT_BUTTON = document.getElementById("LEFT_BUTTON");
 const RIGHT_BUTTON = document.getElementById("RIGHT_BUTTON");
@@ -18,17 +10,15 @@ let activeSet = ALL_POSTER_SETS[chosenSet];
 
 function changePosters(set){
     activeSet = ALL_POSTER_SETS[set];
-    for (i = 0; i < ALL_POSTER_SLOTS.length; i++){
-        ALL_POSTER_SLOTS[i].src = activeSet[i];
+    for (i = 0; i < POSTER_SLOTS.length; i++){
+        POSTER_SLOTS[i].src = activeSet[i];
     }
 }
 
 RIGHT_BUTTON.addEventListener("click", () => {
-    chosenSet = (chosenSet < 4 ? ++chosenSet : chosenSet = 0);
-    changePosters(chosenSet);
+    changePosters(chosenSet < 4 ? ++chosenSet : chosenSet = 0);
 })
 
 LEFT_BUTTON.addEventListener("click", () => {
-    chosenSet = (chosenSet > 0 ? --chosenSet : chosenSet = 4);
-    changePosters(chosenSet);
+    changePosters(chosenSet > 0 ? --chosenSet : chosenSet = 4);
 })
